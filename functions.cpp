2951cb25b03e4e430f793600df5a86c2febf2ca8
CoF-Luke <vladpilot.new@gmail.com>
2025-05-22 22:14:19 +0300

    Updated o/i_streams. Dont work

--- functions.cpp
+++ functions.cpp
@@ -4,18 +4,53 @@
 vector<string> read_from_files(string file_info, string file_key)
 {
     vector<string> data(2);
+    /*char sign;
 
     ifstream bin(file_info);
-    bin >> data[0];
+    while (bin.peek() != EOF)
+    {
+        bin.read((char*)&sign, sizeof(sign));
+        data[0] += sign;
+    }
+    bin.close();*/
+    ifstream bin(file_info, ios::binary);
+
+    bin.seekg(0, ios::end);
+    streampos file_size = bin.tellg();
+    bin.seekg(0, ios::beg);
+    
+    data[0].resize(file_size);
+
+    bin.read(&data[0][0], file_size);
+
     bin.close();
 
-    ifstream bin2(file_key);
-    bin2 >> data[1];
+    cout << data[0] << endl;
+    /*ifstream bin2(file_key);
+    while (bin2.peek() != EOF)
+    {
+        bin2.read((char*)&sign, sizeof(sign));
+        data[1] += sign;
+    }
+    bin2.close();*/
+
+    ifstream bin2(file_info, ios::binary);
+
+    bin2.seekg(0, ios::end);
+    file_size = bin2.tellg();
+    bin2.seekg(0, ios::beg);
+    
+    data[1].resize(file_size);
+
+    bin2.read(&data[1][0], file_size);
+
     bin2.close();
 
     //Копирование входных данный в другой файл для недопущения случайной потери данных...
-    ofstream bout("file_copy_input_info");
-    bout << data[0];
+    ofstream bout(file_info, ios::binary);
+    
+    bout.write(data[0].data(), data[0].size());
+
     bout.close();
 
     return data;
@@ -23,8 +58,18 @@ vector<string> read_from_files(string file_info, string file_key)
 
 void write_to_file(string file_info, string info)
 {
-    ofstream bout(file_info);
+    cout << info << endl;
+    ofstream bout(file_info, ios::binary);
+    
+    //bout.write(info.data(), info.size());
     bout << info;
+
+    bout.close();
+    /*for (size_t i = 0; i < info.length(); ++i)
+    {
+        bout.write((char*)&info[i], sizeof(info[i]));
+    }
+    bout.close();*/
 }
 
 void encr_block(string file_info, string file_key, Encryptor * encr)
